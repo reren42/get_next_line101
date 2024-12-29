e30893305f72b725ed7ddf7f8b5edac7636ff591
Ercan Eren <reren@Ercan-MacBook-Air.local>
2024-12-29 14:14:20 +0300

    ercan

--- ft_get_next_line.c
+++ ft_get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: reren <reren@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 11:53:35 by reren             #+#    #+#             */
-/*   Updated: 2024/12/16 12:42:17 by reren            ###   ########.fr       */
+/*   Updated: 2024/12/29 06:58:18 by reren            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,101 @@ static int ft_thatsit(char *s, int fd)
     temp = malloc((BUFFER_SIZE + 1) * sizeof(char));
     if(!s)
         return(NULL);
-    while(s[i])
-    {
-    }
+    i = -1;
+	while (s[++i] != '\0')
+		if (s[i] == '\n')
+			return (1);
+	return (0);
+}
+
+static char	*ft_hold(char *hold, char *buf)
+{
+	char	*res;
+
+	res = 0;
+	if (!hold && buf)
+	{
+		res = ft_strdup(buf);
+		if (!res)
+			return (NULL);
+		return (res);
+	}
+	res = ft_strjoin(hold, buf);
+	free(hold);
+	return (res);
+}
+
+static char	*ft_line_size(char *hold)
+{
+	char	*line;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (!hold)
+		return (0);
+	while (hold[i] != '\n')
+		i++;
+	line = malloc(sizeof(char) * (i + 2));
+	if (!line)
+		return (0);
+	j = 0;
+	while (j < i + 1)
+	{
+		line[j] = hold[j];
+		j++;
+	}
+	line[j] = '\0';
+	return (line);
+}
+// artık veri line'a kopyalandı.
+static char	*ft_recreate(char *hold)
+{
+	size_t	i;
+	char	*res;
+
+	i = 0;
+	if (!hold)
+		return (NULL);
+	while (hold[i] != '\n')
+		i++;
+	if (hold[i + 1] == '\0')
+		return (free(hold));
+	res = ft_substr(hold, i + 1, ft_strlen(hold));
+	if (!res)
+	{
+		free(hold);
+		return (NULL);
+	}
+	free(hold);
+	return (res);
+}
+
+char	*get_next_line(int fd)
+{
+	char		buf[BUFFER_SIZE + 1];
+	long		bytes_read;
+	static char	*hold = NULL;
+	char		*line;
+
+	line = 0;
+	bytes_read = BUFFER_SIZE;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (free(hold));
+	while (bytes_read > 0)
+	{
+		bytes_read = read(fd, buf, BUFFER_SIZE);
+		if ((bytes_read <= 0 && !hold) || bytes_read == -1)
+			return (free(hold));
+		buf[bytes_read] = '\0';
+		hold = ft_hold(hold, buf);
+		if (ft_thatsit(hold, 0))
+		{
+			line = ft_line_size(hold);
+			if (!line)
+				return (free(hold));
+			return (hold = ft_recreate(hold), line);
+		}
+	}
+	return (free(hold));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: reren <reren@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 16:50:48 by reren             #+#    #+#             */
-/*   Updated: 2024/12/17 18:06:04 by reren            ###   ########.fr       */
+/*   Updated: 2024/12/29 06:57:03 by reren            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE
 # define GET_NEXT_LINE
 #ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 4
+#  define BUFFER_SIZE 11
 #endif
 
 #include <stddef.h>
@@ -23,6 +23,14 @@ char	*ft_strdup(const char *s);
 char *ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(const char *s, unsigned int start, size_t len);
 char    *ft_strchr(const char *s, int c);
+static int ft_thatsit(char *s, int fd);
+static char	*ft_hold(char *hold, char *buf);
+static char	*ft_line_size(char *hold);
+static char	*ft_recreate(char *hold);
+char	*get_next_line(int fd);
+
+
+
 
 
 #endif
